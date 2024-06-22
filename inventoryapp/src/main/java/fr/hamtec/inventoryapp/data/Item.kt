f3c7795ce232bd3b874cd6c8b28e563281132940
HamidB <ejecteur@yahoo.fr>
2024-06-22 08:59:51 +0200

    5. Créer une entité d'article

--- inventoryapp/src/main/java/fr/hamtec/inventoryapp/data/Item.kt
+++ inventoryapp/src/main/java/fr/hamtec/inventoryapp/data/Item.kt
@@ -1,12 +1,21 @@
 package fr.hamtec.inventoryapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 /**
- * Entity data class represents a single row in the database.
+ * La classe de données d'entité représente une seule ligne dans la base de données.
  */
-class Item(
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+    @ColumnInfo(name = "name")
     val name: String,
+    @ColumnInfo(name = "price")
     val price: Double,
+    @ColumnInfo(name = "quantity")
     val quantity: Int
 )
