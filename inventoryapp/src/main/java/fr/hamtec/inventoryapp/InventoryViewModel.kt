465f89a169e07a75f12e849b2b14898d72395da3
HamidB <ejecteur@yahoo.fr>
2024-06-22 11:03:42 +0200

    8. Ajouter un ViewModel

---
+++ inventoryapp/src/main/java/fr/hamtec/inventoryapp/InventoryViewModel.kt
@@ -0,0 +1,38 @@
+package fr.hamtec.inventoryapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import fr.hamtec.inventoryapp.data.Item
+import fr.hamtec.inventoryapp.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(name = itemName, price = itemPrice.toDouble(), quantity = itemCount.toInt() )
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Classe ViewModel inconnue")
+    }
+
+}
