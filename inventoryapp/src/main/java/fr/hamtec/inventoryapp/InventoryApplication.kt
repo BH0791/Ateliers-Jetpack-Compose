8905f5f0383ee050cdd1e1ac323626dd353854c5
HamidB <ejecteur@yahoo.fr>
2024-06-22 10:20:53 +0200

    7. Créer une instance de base de données

--- inventoryapp/src/main/java/fr/hamtec/inventoryapp/InventoryApplication.kt
+++ inventoryapp/src/main/java/fr/hamtec/inventoryapp/InventoryApplication.kt
@@ -4,13 +4,16 @@ package fr.hamtec.inventoryapp
 import android.app.Application
 import fr.hamtec.inventoryapp.data.AppContainer
 import fr.hamtec.inventoryapp.data.AppDataContainer
+import fr.hamtec.inventoryapp.data.ItemRoomDatabase
 
 class InventoryApplication : Application() {
 
     /**
-     * AppContainer instance used by the rest of classes to obtain dependencies
+     * instance d'AppContainer utilisée par le reste des classes pour obtenir des dépendances
      */
     lateinit var container: AppContainer
+
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
 
     override fun onCreate() {
         super.onCreate()
