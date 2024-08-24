4ae402ca5a67f0209dc3b0d0d72bd6c9f09b843f
HamidB <72698487+BH0791@users.noreply.github.com>
2024-08-24 20:05:00 +0200

    Update StartOrderScreen.kt

--- trainingcupcake/src/main/java/fr/hamtec/cupcake/ui/StartOrderScreen.kt
+++ trainingcupcake/src/main/java/fr/hamtec/cupcake/ui/StartOrderScreen.kt
@@ -69,7 +69,7 @@ fun StartOrderScreen(
             quantityOptions.forEach { item ->
                 SelectQuantityButton(
                     labelResourceId = item.first,
-                    onClick = {}
+                    onClick = {onNextButtonClicked(item.second)} 
                 )
             }
         }
