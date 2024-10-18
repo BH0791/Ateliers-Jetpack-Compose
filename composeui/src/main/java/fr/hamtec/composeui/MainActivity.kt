37b0f1c56ee32e21d6e0fae00683f31cdb918657
HamidB <ejecteur@yahoo.fr>
2024-10-18 16:16:56 +0200

    Update

--- composeui/src/main/java/fr/hamtec/composeui/MainActivity.kt
+++ composeui/src/main/java/fr/hamtec/composeui/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import fr.hamtec.composeui.livre.SlotAllowCustomComponent
+import fr.hamtec.composeui.livre.SmallTopAppBarExample
 import fr.hamtec.composeui.ui.theme.AteliersJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AteliersJetpackComposeTheme() {
-                    SlotAllowCustomComponent()
+                SmallTopAppBarExample()
             }
         }
     }
--- composeui/src/main/java/fr/hamtec/composeui/livre/SupportJCB.kt
+++ composeui/src/main/java/fr/hamtec/composeui/livre/SupportJCB.kt
@@ -2,12 +2,16 @@ package fr.hamtec.composeui.livre
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,12 +47,51 @@ fun SlotAllowCustomComponent() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SmallTopAppBarExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                }
+            )
+        },
+    ) { innerPadding ->
+        ScrollContent(innerPadding)
+    }
+}
+@Composable
+fun ScrollContent(innerPadding: PaddingValues) {
+    val range = 1..100
 
-
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = innerPadding,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(range.count()) { index ->
+            Text(text = "- List item number ${index + 1}")
+        }
+    }
+}
 @Preview
 @Composable
 private fun SlotAllowCustomComponentPreview() {
     AteliersJetpackComposeTheme {
         SlotAllowCustomComponent()
+    }
+}
+@Preview
+@Composable
+private fun SmallTopAppBarExamplePreview() {
+    AteliersJetpackComposeTheme {
+        SmallTopAppBarExample()
     }
 }
