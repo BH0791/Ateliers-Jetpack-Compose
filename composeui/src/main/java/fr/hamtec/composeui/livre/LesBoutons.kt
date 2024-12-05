c8e3bbfaf52d428de80c6f2b3f44941e6672913b
HamidB <ejecteur@yahoo.fr>
2024-12-05 15:09:05 +0100

    Update Button

--- composeui/src/main/java/fr/hamtec/composeui/livre/LesBoutons.kt
+++ composeui/src/main/java/fr/hamtec/composeui/livre/LesBoutons.kt
@@ -1,11 +1,14 @@
 package fr.hamtec.composeui.livre
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.ConnectedTv
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,19 +17,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import fr.hamtec.composeui.ui.theme.AteliersJetpackComposeTheme
 
 @Composable
 fun SimpleBouton(): Unit {
     Button(
-        onClick = {/* TODO */},
-        content = {Text(text = "Button")}
+        onClick = {/* TODO */ },
+        content = { Text(text = "Button") }
     )
 }
+
 @Composable
 fun DifBouton(): Unit {
     Button(
-        onClick = {/* TODO */},
+        onClick = {/* TODO */ },
+        shape = MaterialTheme.shapes.small,
         content = {
             Icon(
                 Icons.Default.ConnectedTv,
@@ -35,7 +39,7 @@ fun DifBouton(): Unit {
             )
             Column(
                 modifier = Modifier.weight(1f)
-            ){
+            ) {
                 Text(
                     text = "Lecture en cours - 90%",
                     textAlign = TextAlign.Center,
@@ -44,27 +48,91 @@ fun DifBouton(): Unit {
                     text = "TV salon",
                     style = MaterialTheme.typography.bodySmall,
                 )
-                Icon(
-                    Icons.Default.ChevronRight,
-                    contentDescription = null,
-                    modifier= Modifier.padding(start = 16.dp),
-                )
+
             }
+            Icon(
+                Icons.Default.ChevronRight,
+                contentDescription = null,
+                modifier = Modifier.padding(start = 16.dp),
+            )
         }
     )
 }
-//----->Preview
-@Preview(showBackground = true)
+
 @Composable
-fun SimpleBoutonPreview(){
+fun DifBouton2(): Unit {
+    Button(
+        modifier = Modifier.height(80.dp),
+        shape = MaterialTheme.shapes.medium,
+        onClick = {/* TODO */ }
+    ) {
+        Column {
+            Icon(
+                Icons.Default.ConnectedTv,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 16.dp),
+            )
+            Text(
+                text = "Lecture en cours - 90%",
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "TV salon",
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+}
+
+@Composable
+fun DifBouton3(): Unit {
+    Button(
+        modifier = Modifier.height(80.dp),
+        shape = MaterialTheme.shapes.medium,
+        onClick = {/* TODO */ },
+        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 6.dp),
+        contentPadding = PaddingValues(4.dp),
+        colors = ButtonDefaults.buttonColors(containerColor  = MaterialTheme.colorScheme.secondary )
+    ) {
+        Column {
+            Icon(
+                Icons.Default.ConnectedTv,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 16.dp),
+            )
+            Text(
+                text = "Lecture en cours - 90%",
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "TV salon",
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+}
+
+//----->Preview
+@Preview(
+    showBackground = true
+)
+@Composable
+fun SimpleBoutonPreview() {
     SimpleBouton()
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun DifBoutonPreview() {
-    AteliersJetpackComposeTheme {
-        DifBouton()
+    DifBouton()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DifBouton2Preview() {
+    Column {
+        DifBouton2()
+        DifBouton3()
     }
 }
 
--- composeui/src/main/java/fr/hamtec/composeui/ui/theme/Theme.kt
+++ composeui/src/main/java/fr/hamtec/composeui/ui/theme/Theme.kt
@@ -48,7 +48,7 @@ private val LightColorScheme = lightColorScheme(
 fun AteliersJetpackComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -64,7 +64,7 @@ fun AteliersJetpackComposeTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = MyTypography,
-        content = content,
-        shapes = Shapes
+        shapes = Shapes,
+        content = content
     )
 }
